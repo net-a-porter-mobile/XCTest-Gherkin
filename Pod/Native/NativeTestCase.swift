56190f7f416c18a452d5fb203e01aa6a7fbba9c4
Rabursky <rabursky@gmail.com>
2016-09-19 09:34:57 +0100

    registerTestMethod success assertion added

--- Pod/Native/NativeTestCase.swift
+++ Pod/Native/NativeTestCase.swift
@@ -117,6 +117,7 @@ open class NativeTestCase : XCTestCase {
         let selector = sel_registerName(scenario.selectorCString)
         let method = class_getInstanceMethod(self, #selector(featureScenarioTest))
         let success = class_addMethod(self, selector, method_getImplementation(method), method_getTypeEncoding(method))
+        assert(success, "Could not swizzle feature test method!")
     }
     
 }
