82a9e7240cdb7bb1c9cfeb222adf383643b24754
Jacques de Vos <jacdevos@gmail.com>
2016-11-15 16:35:55 +0200

    Create native feature runner that can explicitly run a scenario from a test

---
+++ Example/Tests/Features/ExampleNativeRunnerTest.swift
@@ -0,0 +1,22 @@
+//
+//  ExampleNativeRunnerTest.swift
+//  XCTest-Gherkin
+//
+//  Created by jacdevos on 2016/11/15.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import XCTest_Gherkin
+
+class ExampleNativeRunnerTest: XCTestCase {
+
+    func testNativeExampleWithFeatureRunner() {
+        NativeRunner.runFeature(featureFile: "NativeFeatures/native_example.feature", testCase: self)
+    }
+    
+    func testNativeExampleWithScenarioRunner() {
+        NativeRunner.runScenario(featureFile: "NativeFeatures/native_example.feature", scenario: "This is a basic happy path example", testCase: self)
+    }
+    
+}
