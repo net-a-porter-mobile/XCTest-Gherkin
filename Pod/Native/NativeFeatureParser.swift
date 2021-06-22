4966f44dc4309a2b195f161f76468bc510613b2e
tomleightonstars <77737913+tomleightonstars@users.noreply.github.com>
2021-06-22 13:43:34 +0100

    Add tag ignoring functionality

--- Pod/Native/NativeFeatureParser.swift
+++ Pod/Native/NativeFeatureParser.swift
@@ -56,6 +56,20 @@ struct NativeFeatureParser {
             return nil
         }
 
+        // Note: Ignored tags take precendence over 'wanted' tags
+        if let userIgnoreTagsArgumentIndex = CommandLine.arguments.firstIndex(where: { $0.hasPrefix("IgnoreTags=") }) {
+            let userIgnoreTags = CommandLine.arguments[userIgnoreTagsArgumentIndex].replacingOccurrences(of: "IgnoreTags=", with: "")
+                                                                       .replacingOccurrences(of: "@", with: "")
+                                                                       .components(separatedBy: ",")
+                                                                       .filter{ !$0.isEmpty }
+            if userIgnoreTags.isEmpty { return feature }
+            for index in (0...feature.scenarios.count-1).reversed() {
+                if feature.scenarios[index].tags.contains(where: userIgnoreTags.contains) {
+                    feature.scenarios.remove(at: index)
+                }
+            }
+        }
+        
         if let userTagsArgumentIndex = CommandLine.arguments.firstIndex(where: { $0.hasPrefix("Tags=") }) {
             let userTags = CommandLine.arguments[userTagsArgumentIndex].replacingOccurrences(of: "Tags=", with: "")
                                                                        .replacingOccurrences(of: "@", with: "")
